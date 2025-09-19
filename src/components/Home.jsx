a2023126107193dd2d434f4681dd67b43d97af40
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-09-19 23:17:23 +0100

    refactor Home component and update styles for improved user experience

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,94 +1,30 @@
 import { useEffect, useMemo, useRef, useState } from "react"
 
-
 export default function HomePage() {
-    const [id, setId] = useState(() => {
-        const storedId = localStorage.getItem("id");
-        return storedId ? JSON.parse(storedId) : 0; // start at 0 if none saved
-    });
-    const [name, setName] = useState("")
-    const [date, setDate] = useState("")
-    const [entry, setEntry] = useState("")
-    const [active, setActive] = useState(() => {
-        const stored = localStorage.getItem("active");
-        return stored ? JSON.parse(stored) : false;
-    });
-    const [items, setItems] = useState(() => {
-        const storedItems = localStorage.getItem("items");
-        return storedItems ? JSON.parse(storedItems) : [];
-    });
-    // const [searchParam, setSearchParam] = useState("")
-    // const [searchItem, setSearchItem] = useState("")
+    const [userName, setUserName] = useState(() => {
+        const storedUser = localStorage.getItem("name")
+        return storedUser ? JSON.parse(storedUser) : ""
+    })
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newItem = { name, date, entry, id };
-        setItems((prevItems) => [...prevItems, newItem]);
-        setName("");
-        setDate("");
-        setEntry("");
-        setId((prevId) => prevId + 1);
-        setActive(true);
-    };
     useEffect(() => {
-        localStorage.setItem("id", JSON.stringify(id));
-    }, [id]);
-    useEffect(() => {
-        localStorage.setItem("active", JSON.stringify(active));
-    }, [active]);
-    useEffect(() => {
-        localStorage.setItem("items", JSON.stringify(items));
-        console.log(items);
-    }, [items]);
+        localStorage.setItem("name", JSON.stringify(userName));
+    },[userName])
+    
 
-
-    // const handleSearch = useMemo(() => {
-    //     return items.filter(item => item.name.toLowerCase().includes(searchParam.toLowerCase()))
-    // }, [items, searchParam])
 
     return (
-        <div className="parent">
-            {/* <h1>Bored, Here's a form, please input the appropriate info and click submit</h1> */}
-            {/* <nav className="navigation">
+        <div className="parent">            
+            <nav className="navigation">
                 <ul>
                     <li>Home</li>
                     <li>Books</li>
                     <li>Authors</li>
                 </ul>
-            </nav> */}
+            </nav>
             <div className="card">
-                <h1>Bored, Here's a form, please input the appropriate info and click submit</h1>
-                <div className="inputs">
-                    <form onSubmit={handleSubmit}>
-                        <div className="split">
-                            <input className="date-form" type="text" placeholder="Enter Your Entry Title" value={name} onChange={(e) => setName(e.target.value)} required />
-                            <input type="datetime-local" value={date} onChange={(e) => setDate(e.target.value)} required />
-                        </div>
-                        <textarea type="text" placeholder="Make Your Entry" value={entry} onChange={(e) => setEntry(e.target.value)} required />
-                        <button>Save</button>
-                    </form>
-                </div>
-                <div className={`search ${active ? "active" : ""}`}>
-                    {items.map((item, idx) => {
-                        return (
-                            <div key={item.id} className="results">
-                                <h1>{item.name}</h1>
-                                <p className="text date">Entry made at {item.date}</p>
-                                <p className="text">{item.entry}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-                {/* <input type="text" placeholder="search" value={searchParam} onChange={(e) => setSearchParam(e.target.value)} />
-                        {handleSearch.map((item) => {
-                            return (
-                                <div key={item.id} className="search">
-                                    <p>{item.name}</p>
-                                    <p>{item.age}</p>
-                                    <p>{item.club}</p>
-                                </div>
-                            )
-                        })} */}
+                <h1>Back to base</h1>
+                <p>{userName}</p>
+                <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} />
             </div>
 
         </div>
