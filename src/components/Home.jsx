81f016a51c41a3eb890134ff06380ff716085e19
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-09-26 22:32:41 +0100

    refactor Home component to manage debtors with improved form handling and theme toggle functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,74 +1,90 @@
 import { useEffect, useMemo, useRef, useState } from "react"
 
 export default function HomePage() {
-    const myRef = useRef()
-
-    const changeThings = () => {
-        const change = myRef.current
-        change.style.color = "red"
-        change.style.transition = "all .3s ease-in-out"
-        change.innerText = "I have been changed"
-    }
-    useEffect(() => {
-        console.log(myRef.current)
-    }, [myRef])
-
-    const [users, setUsers] = useState(() => {
-        const stored = localStorage.getItem("list")
-        return stored ? JSON.parse(stored) : []
+    const [theme, setTheme] = useState(() => {
+        const stored = localStorage.getItem("theme")
+        return stored ? JSON.parse(stored) : false
     })
-    
     useEffect(() => {
-        localStorage.setItem("list", JSON.stringify(users))
-    }, [users])
+        localStorage.setItem("theme", JSON.stringify(theme))
+    }, [theme])
+    const toggleTheme = () => {
+        setTheme(!theme)
+    }
 
+    const myRef = useRef()
     const [id, setId] = useState()
     const [name, setName] = useState("")
-    const [age, setAge] = useState("")
-    const [level, setLevel] = useState("")
+    const [amount, setAmount] = useState("")
+    const [reason, setReason] = useState("")
+    const [debtors, setDebtors] = useState(() => {
+        const stored = localStorage.getItem("debtors")
+        return stored ? JSON.parse(stored) : []
+    })
+    useEffect(() => {
+        localStorage.setItem("debtors", JSON.stringify(debtors))
+    }, [debtors])
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        const list = { name, age, level, id: Math.random() }
-        setUsers((prev) => [...prev, list])
-        setName("")
-        setAge("")
-        setLevel("")
-        setId("")
-        alert("submitted")
-        console.log(users);
+        const list = { name, amount, reason, id: Math.random() }
+        setDebtors((prev) => [...prev, list])
 
+        setName("")
+        setAmount("")
+        setReason("")
+        setId("")
     }
-    // useEffect(() => {handleSubmit()},[])
+    useEffect(() => {
+        console.log(debtors)
+    }, [debtors])
+    const [searchParam, setSearchParam] = useState("")
+
+    const filteredDebtors = useMemo(() => {
+        return debtors.filter(debtor => debtor.amount >= searchParam)
+    }, [searchParam, debtors])
+
+    useEffect(() => {
+        console.log(filteredDebtors)
+    }, [debtors])
 
     return (
-        <div className="parent">
-            <nav className="navigation">
+        <div className={`parent ${theme ? "" : "active"}`}>
+            {/* <nav className="navigation">
                 <ul>
                     <li>Home</li>
                     <li>Books</li>
-                    <li>Authors</li>
+                    <li onClick={toggleTheme}>Toggle</li>
                 </ul>
-            </nav>
-            <div className="card">
-                <h1 ref={myRef}>Back to base</h1>
-                <button onClick={changeThings}>Change</button>
+            </nav> */}
+            {/* <div className="card"> */}
+            {/* <h1 ref={myRef}>Back to base</h1> */}
+            <div className="forms">
+                <button className="toggle" onClick={toggleTheme}>Toggle</button>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Your Name"/> <br />
-                    <input type="number" value={age} onChange={(e) => setAge(e.target.value)} placeholder="Enter Your Age"/> <br />
-                    <input type="text" value={level} onChange={(e) => setLevel(e.target.value)} placeholder="Enter Your Level"/>
-                    <button type="submit">Submit</button>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Debtor's Name" required />
+                    <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter Amount Owed" required />
+                    <textarea placeholder="Enter Debtor's Reason" value={reason} onChange={(e) => setReason(e.target.value)} required />
+                    <input type="text" value={searchParam} onChange={(e) => setSearchParam(e.target.value)} placeholder="Search For Debtors" />
+                    <button type="submit">save</button>
                 </form>
-
-                <div>
-                    <ul >
-                        {users.map((list) => (
-                                <li key={list.id}> My name is {list.name}, my age is {list.age} and my level is {list.level}</li>
-                        ))}
-                    </ul>
-                </div>
+                {filteredDebtors.length > 0 && (
+                    <div className="debtors">
+                        {filteredDebtors.map((debtor) => {
+                            return (
+                                <div className="debtor" key={debtor.id}>
+                                    <h1>{debtor.name}</h1>
+                                    <p>{debtor.amount}</p>
+                                    <p>{debtor.reason}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                )}
+                { }
             </div>
-
         </div>
+
+        // </div>
     )
 }
